c1f6fd055dd5edf52b991fab4f15ce2efc5829af
bgtti <bru.mgtti@gmail.com>
2025-04-22 01:52:27 +0200

    browser reload extension

--- polln/settings.py
+++ polln/settings.py
@@ -70,9 +70,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-if ENVIRONMENT == "development":
-    INSTALLED_APPS.append("django_browser_reload")
-    MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
+# if ENVIRONMENT == "development":
+#     INSTALLED_APPS.append("django_browser_reload")
+#     MIDDLEWARE.append("django_browser_reload.middleware.BrowserReloadMiddleware")
 
 ROOT_URLCONF = 'polln.urls'
 
