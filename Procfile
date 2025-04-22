826aa26ff097df38eb064a20b7b51225f9a5327c
bgtti <bru.mgtti@gmail.com>
2025-04-22 20:21:35 +0200

    prod version

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn polln.wsgi --log-file -
--- polln/settings/dev_settings.py
+++ polln/settings/dev_settings.py
@@ -2,18 +2,4 @@
 Settings specific to local development.
 Inherits from base settings.
 """
-from .base_settings import *
-
-DEBUG = True
-
-# Define base url (points to localhost in development)
-BASE_URL = "http://127.0.0.1:8000"
-
-ALLOWED_HOSTS = []
-CSRF_TRUSTED_ORIGINS = []
-
-INSTALLED_APPS += ["django_browser_reload"]
-MIDDLEWARE += ["django_browser_reload.middleware.BrowserReloadMiddleware"]
-
-STATIC_ROOT = BASE_DIR / "static_root"
-STATICFILES_DIRS = [ BASE_DIR / "static", ]
+# dev settings deleted in production as not to cause conflicts
