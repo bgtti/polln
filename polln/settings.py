e65d34ac6239e820225ed568805490b0dfd80296
bgtti <bru.mgtti@gmail.com>
2025-04-22 02:13:19 +0200

    django

--- polln/settings.py
+++ polln/settings.py
@@ -34,7 +34,7 @@ if ENVIRONMENT == "production":
 BASE_URL = os.getenv("BASE_URL")
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (ENVIRONMENT == 'development')
