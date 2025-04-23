3cb2e83a7576f9d5622d471f213e5f4bec5bf731
bgtti <bru.mgtti@gmail.com>
2025-04-23 02:06:48 +0200

    http to https link when sharing

--- polln/settings/prod_settings.py
+++ polln/settings/prod_settings.py
@@ -16,3 +16,10 @@ CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(',')
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [ os.path.join(BASE_DIR / "static"), ]
+
+# “If the incoming request has a header X-Forwarded-Proto: https, treat the request as HTTPS.”
+# Without it, share link will contain http instead of https
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+#This will redirect any accidental HTTP request to HTTPS — good for SEO, security, and consistency in links:
+SECURE_SSL_REDIRECT = True
